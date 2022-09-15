01c3bdda2cdeee3a83d986595819cb7fb730f3dc
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2022-09-15 15:43:57 +0530

    fetch using async await and then

--- src/FetchData/Fetch.jsx
+++ src/FetchData/Fetch.jsx
@@ -6,10 +6,11 @@ const FetchData = () => {
 	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
-		fetchData();
+		usingAsyncAwait();
+		usingThen();
 	}, []);
 
-	const fetchData = async () => {
+	const usingAsyncAwait = async () => {
 		try {
 			const response = await fetch(
 				'https://jsonplaceholder.typicode.com/users'
@@ -19,6 +20,7 @@ const FetchData = () => {
 					`This is an HTTP error, The status is ${response.status}`
 				);
 			const users = await response.json();
+			console.log(users);
 			setUser(users);
 			setFetchError(null);
 		} catch (err) {
@@ -28,17 +30,49 @@ const FetchData = () => {
 			setLoading(false);
 		}
 	};
+	const usingThen = () => {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error(
+						`This is an HTTP error, The status is ${response.status}`
+					);
+				}
+				return response.json();
+			})
+			.then((data) => {
+				console.log(data);
+				setUser(data);
+				setFetchError(null);
+			})
+			.catch((err) => {
+				setFetchError(err.message);
+				setUser(null);
+			})
+			.finally(() => {
+				setLoading(false);
+			});
+	};
 
 	return (
 		<div>
 			{loading && <p>Loading....</p>}
 			{fetchError && <p style={{ color: 'red' }}>{`Error: ${fetchError}`}</p>}
 			{!fetchError && !loading && (
-				<>
+				<div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px' }}>
 					{user.map((use) => (
-						<p>{use.name}</p>
+						<div
+							key={use.id}
+							style={{
+								border: '1px solid green',
+								padding: '10px',
+							}}
+						>
+							<p>{use.name}</p>
+							<p>{use.email}</p>
+						</div>
 					))}
-				</>
+				</div>
 			)}
 		</div>
 	);
