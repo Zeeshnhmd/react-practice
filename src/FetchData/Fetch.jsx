19b37a376f3803a12142dc1e9fadd767c9dff5d2
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2022-09-18 13:34:29 +0530

    fetch all

--- src/FetchData/Fetch.jsx
+++ src/FetchData/Fetch.jsx
@@ -3,7 +3,9 @@ import React, { useEffect, useState } from 'react';
 const FetchData = () => {
 	const [user, setUser] = useState(null);
 	const [fetchError, setFetchError] = useState(null);
+	const [fetchAllFetchError, setFetchAllFetchError] = useState(null);
 	const [loading, setLoading] = useState(true);
+	const [fetchAllLoading, setFetchAllLoading] = useState(true);
 	const [post, setPost] = useState([]);
 	const [comment, setComment] = useState([]);
 	const [album, setAlbum] = useState([]);
@@ -61,39 +63,75 @@ const FetchData = () => {
 				setLoading(false);
 			});
 	};
-
-	// FetchAll data
+	/**
+	 * FetchAll data
+	 * TODO:
+	 * - MAKE THE CODE MORE CLEAN
+	 *
+	 */
 
 	const fetchAllData = async () => {
 		const posts = fetch(
 			'https://jsonplaceholder.typicode.com/posts?_start=0&_limit=10'
-		).then((res) => res.json());
+		).then((res) => {
+			if (!res.ok) {
+				throw new Error(`This is an HTTP error, The status is ${res.status}`);
+			}
+			return res.json();
+		});
 
 		const comments = fetch(
 			'https://jsonplaceholder.typicode.com/comments?_start=0&_limit=10'
-		).then((res) => res.json());
+		).then((res) => {
+			if (!res.ok) {
+				throw new Error(`This is an HTTP error, The status is ${res.status}`);
+			}
+			return res.json();
+		});
 
 		const albums = fetch(
 			'https://jsonplaceholder.typicode.com/albums?_start=0&_limit=10'
-		).then((res) => res.json());
+		).then((res) => {
+			if (!res.ok) {
+				throw new Error(`This is an HTTP error, The status is ${res.status}`);
+			}
+			return res.json();
+		});
 
 		return Promise.all([posts, comments, albums]);
 	};
 
 	useEffect(() => {
-		fetchAllData().then((res) => {
-			const postData = res[0];
-			const commentData = res[1];
-			const albumData = res[2];
+		fetchAllData()
+			.then((res) => {
+				const postData = res[0];
+				const commentData = res[1];
+				const albumData = res[2];
 
-			setPost(postData);
-			setComment(commentData);
-			setAlbum(albumData);
-		});
+				console.log(postData, 'post');
+				console.log(commentData, 'comment');
+				console.log(albumData, 'album');
+
+				setPost(postData);
+				setComment(commentData);
+				setAlbum(albumData);
+				setFetchAllFetchError(null);
+			})
+			.catch((err) => {
+				setFetchAllFetchError(err.message);
+				setPost(null);
+				setComment(null);
+				setAlbum(null);
+			})
+			.finally(() => {
+				setFetchAllLoading(false);
+			});
 	}, []);
 
 	return (
 		<>
+			{/* usingAsynAwait and usingThen */}
+
 			<div>
 				{loading && <p>Loading....</p>}
 				{fetchError && <p style={{ color: 'red' }}>{`Error: ${fetchError}`}</p>}
@@ -115,24 +153,31 @@ const FetchData = () => {
 				)}
 			</div>
 
-			{/* FetchAll */}
-			<div style={{ display: 'flex', gap: '20px', marginTop: '50px' }}>
-				<div style={{ border: '1px solid red', padding: '10px' }}>
-					{post.map((al) => (
-						<p> {al.title}</p>
-					))}
+			{/* fetchAllData */}
+			{fetchAllLoading && <p>FetchAll Loading...</p>}
+
+			{!fetchAllLoading && !fetchError && (
+				<div style={{ display: 'flex', gap: '20px', marginTop: '50px' }}>
+					<div style={{ border: '1px solid red', padding: '10px' }}>
+						{fetchAllFetchError && <p>{`Error: ${fetchAllFetchError}`}</p>}
+						{post?.map((al) => (
+							<p> {al.title}</p>
+						))}
+					</div>
+					<div style={{ border: '1px solid green', padding: '10px' }}>
+						{fetchAllFetchError && <p>{`Error: ${fetchAllFetchError}`}</p>}
+						{comment?.map((al) => (
+							<p> {al.name}</p>
+						))}
+					</div>
+					<div style={{ border: '1px solid blue', padding: '10px' }}>
+						{fetchAllFetchError && <p>{`Error: ${fetchAllFetchError}`}</p>}
+						{album?.map((al) => (
+							<p> {al.title}</p>
+						))}
+					</div>
 				</div>
-				<div style={{ border: '1px solid green', padding: '10px' }}>
-					{comment.map((al) => (
-						<p> {al.name}</p>
-					))}
-				</div>
-				<div style={{ border: '1px solid blue', padding: '10px' }}>
-					{album.map((al) => (
-						<p> {al.title}</p>
-					))}
-				</div>
-			</div>
+			)}
 		</>
 	);
 };
