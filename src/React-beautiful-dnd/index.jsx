201f60480f9a7c95ea1cf63aa25ead7e7c44bd4b
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2022-12-11 18:41:50 +0530

    feat: react beautiful DND

--- src/React-beautiful-dnd/index.jsx
+++ src/React-beautiful-dnd/index.jsx
@@ -1,53 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import styles from "./reactBeautifulDnd.module.scss";
 
 const ReactBeautifulDnd = () => {
+  // const data = [
+  //   {
+  //     id: "1",
+  //     name: "zeeshan",
+  //     location: "JSR",
+  //     avatar: "https://robohash.org/YOUR-TEXT.png",
+  //   },
+  //   {
+  //     id: "2",
+  //     name: "Arpit",
+  //     location: "DHAN",
+  //     avatar: "https://robohash.org/YOUR-TEXT.png",
+  //   },
+  //   {
+  //     id: "3",
+  //     name: "Ujjwal",
+  //     location: "ASSAM",
+  //     avatar: "https://robohash.org/YOUR-TEXT.png",
+  //   },
+  //   {
+  //     id: "4",
+  //     name: "Reetool",
+  //     location: "J&K",
+  //     avatar: "https://robohash.org/YOUR-TEXT.png",
+  //   },
+  //   {
+  //     id: "5",
+  //     name: "Rayyan",
+  //     location: "RAN",
+  //     avatar: "https://robohash.org/YOUR-TEXT.png",
+  //   },
+  // ];
+
+  //TODO: React beautiful DND
+
   const data = [
     {
       id: "1",
-      name: "zeeshan",
-      location: "JSR",
-      avatar: "https://robohash.org/YOUR-TEXT.png",
+      title: "Shortlisted",
+      cadidate: [],
     },
-    {
-      id: "2",
-      name: "Arpit",
-      location: "DHAN",
-      avatar: "https://robohash.org/YOUR-TEXT.png",
-    },
-    {
-      id: "3",
-      name: "Ujjwal",
-      location: "ASSAM",
-      avatar: "https://robohash.org/YOUR-TEXT.png",
-    },
-    {
-      id: "4",
-      name: "Reetool",
-      location: "J&K",
-      avatar: "https://robohash.org/YOUR-TEXT.png",
-    },
-    {
-      id: "5",
-      name: "Rayyan",
-      location: "RAN",
-      avatar: "https://robohash.org/YOUR-TEXT.png",
-    },
+    { id: "2", title: "Cultural evaluation" },
+    { id: "3", title: "Technical interview" },
+    { id: "4", title: "Final discussion" },
   ];
+  const [card, setCard] = useState(data);
+
+  const handleOnDragEnd = (result) => {
+    console.log(result);
+    if (!result.destination) return;
+    const items = Array.from(card);
+    const [recordedItems] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, recordedItems);
+
+    setCard(items);
+  };
 
   return (
     <div className={styles["wrapper"]}>
-      <div className={styles["cards-wrapper"]}>
-        {data.map((el) => (
-          <div key={el.id} className={styles["card"]}>
-            <img className={styles["avatar"]} src={el.avatar} alt="avatar" />
-            <div>
-              <h1 className={styles["name"]}>Name: {el.name}</h1>
-              <p className={styles["address"]}>Location: {el.location}</p>
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId="cards">
+          {(provided) => (
+            <div
+              className={styles["cards-wrapper"]}
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {card.map((el, index) => (
+                <Draggable key={el.id} draggableId={el.id} index={index}>
+                  {(provided) => (
+                    <div
+                      className={styles["card"]}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      ref={provided.innerRef}
+                    >
+                      <img
+                        className={styles["avatar"]}
+                        src={el.avatar}
+                        alt="avatar"
+                      />
+                      <div>
+                        <h1 className={styles["name"]}>Name: {el.name}</h1>
+                        <p className={styles["address"]}>
+                          Location: {el.location}
+                        </p>
+                      </div>
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
             </div>
-          </div>
-        ))}
-      </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 };
