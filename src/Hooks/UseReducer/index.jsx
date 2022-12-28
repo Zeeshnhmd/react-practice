fa8f24a64349a60b1e2d22000512d93967fc7fa6
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2022-12-28 12:38:27 +0530

    fix: reducer fixes

--- src/Hooks/UseReducer/index.jsx
+++ src/Hooks/UseReducer/index.jsx
@@ -1,51 +1,53 @@
-import React from 'react';
-import axios from 'axios';
-import { useReducer } from 'react';
-import { useEffect } from 'react';
+import React from "react";
+import axios from "axios";
+import { useReducer } from "react";
+import { useEffect } from "react";
 
 const initialState = {
-	loading: true,
-	post: [],
-	error: '',
+  loading: true,
+  post: [],
+  error: "",
 };
 
 const reducer = (state, action) => {
-	switch (action.type) {
-		case 'FETCH_SUCCESS':
-			return {
-				loading: false,
-				post: action.payload,
-				error: '',
-			};
-		case 'FETCH_ERROR':
-			return {
-				loading: false,
-				post: [],
-				error: 'Something went wrong',
-			};
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case "FETCH_SUCCESS":
+      return {
+        loading: false,
+        post: action.payload,
+        error: "",
+      };
+    case "FETCH_ERROR":
+      return {
+        loading: false,
+        post: [],
+        error: "Something went wrong",
+      };
+    default:
+      return state;
+  }
 };
+
 const ReducerHook = () => {
-	const [state, dispatch] = useReducer(reducer, initialState);
-	useEffect(() => {
-		axios
-			.get('https://jsonplaceholder.typicode.com/posts')
-			.then((res) => {
-				dispatch({ type: 'FETCH_SUCCESS', payload: res.data });
-			})
-			.catch((err) => {
-				dispatch({ type: 'FETCH_ERROR' });
-			});
-	}, []);
-	return (
-		<div>
-			{/* {state.loading ? 'Loading...' : state.post.title} */}
-			{state.loading ? 'Loading...' : state.post.map((po) => <p>{po.title}</p>)}
-			{state.error && state.error}
-		</div>
-	);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => {
+        dispatch({ type: "FETCH_SUCCESS", payload: res.data });
+      })
+      .catch((err) => {
+        dispatch({ type: "FETCH_ERROR" });
+      });
+  }, []);
+
+  return (
+    <div>
+      {/* {state.loading ? 'Loading...' : state.post.title} */}
+      {state.loading ? "Loading..." : state.post.map((po) => <p>{po.title}</p>)}
+      {state.error && state.error}
+    </div>
+  );
 };
 
 export default ReducerHook;
